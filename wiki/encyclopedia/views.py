9e56b6817334c394b81617020aa633a07d55bb2c
Derek Marchese <derekmarchese1@gmail.com>
2024-01-11 11:29:03 -0500

    function for md to html;

--- wiki/encyclopedia/views.py
+++ wiki/encyclopedia/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import render
+from markdown2 import Markdown
 
 from . import util
+
+#function to convert md to html
+def convert_md_to_html(title):
+    content = util.get_entry(title)
+    markdowner = Markdown()
+    if content == None:
+        return None
+    else:
+        return markdowner.convert(content)
 
 
 def index(request):
